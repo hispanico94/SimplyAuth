1882b4e549866307ee259c31869217f5d8a1c23a
Paolo Rocca <hispanico94@gmail.com>
2020-08-01 18:18:40 +0200

    Created Xcode project

---
+++ SimplyAuth/SimplyAuth/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SimplyAuth
+//
+//  Created by Paolo Rocca on 01/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimplyAuth/SimplyAuth/SimplyAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyAuthApp.swift
+//  SimplyAuth
+//
+//  Created by Paolo Rocca on 01/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SimplyAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
