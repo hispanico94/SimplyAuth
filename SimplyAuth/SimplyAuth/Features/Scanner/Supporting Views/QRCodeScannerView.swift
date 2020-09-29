7213943a522061654ecf32ebb5844166f64de4d0
Paolo Rocca <hispanico94@gmail.com>
2020-09-29 08:43:38 +0200

    Defined supporting views for Scanner feature

---
+++ SimplyAuth/SimplyAuth/Features/Scanner/Supporting Views/QRCodeScannerView.swift
@@ -0,0 +1,36 @@
+//
+//  QRCodeScannerView.swift
+//  SimplyAuth
+//
+//  Created by Paolo Rocca on 29/09/2020.
+//
+
+import SwiftUI
+
+struct QRCodeScannerView: UIViewControllerRepresentable {
+  final class Coordinator: QRCodeScannerViewControllerDelegate {
+    private let parent: QRCodeScannerView
+    
+    init(_ parent: QRCodeScannerView) {
+      self.parent = parent
+    }
+    
+    func qrCodeScannerViewController(_ viewController: QRCodeScannerViewController, didFoundQRCode qrCode: String) {
+      parent.qrCode = qrCode
+    }
+  }
+  
+  @Binding var qrCode: String
+  
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+  
+  func makeUIViewController(context: Context) -> QRCodeScannerViewController {
+    let vc = QRCodeScannerViewController()
+    vc.delegate = context.coordinator
+    return vc
+  }
+  
+  func updateUIViewController(_ uiViewController: QRCodeScannerViewController, context: Context) { }
+}
---
+++ SimplyAuth/SimplyAuth/Features/Scanner/Supporting Views/QRCodeScannerViewController.swift
@@ -0,0 +1,123 @@
+//
+//  QRCodeScannerViewController.swift
+//  SimplyAuth
+//
+//  Created by Paolo Rocca on 29/09/2020.
+//
+
+import AVFoundation
+import Combine
+import UIKit
+
+protocol QRCodeScannerViewControllerDelegate: AnyObject {
+  func qrCodeScannerViewController(_ viewController: QRCodeScannerViewController, didFoundQRCode qrCode: String)
+}
+
+final class QRCodeScannerViewController: UIViewController {
+  private var captureSession: AVCaptureSession!
+  private var previewLayer: AVCaptureVideoPreviewLayer!
+  
+  weak var delegate: QRCodeScannerViewControllerDelegate?
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+  
+  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    return .portrait
+  }
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = UIColor.black
+    captureSession = AVCaptureSession()
+    
+    guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+    let videoInput: AVCaptureDeviceInput
+    
+    do {
+      videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+    } catch {
+      return
+    }
+    
+    if (captureSession.canAddInput(videoInput)) {
+      captureSession.addInput(videoInput)
+    } else {
+      failed()
+      return
+    }
+    
+    let metadataOutput = AVCaptureMetadataOutput()
+    
+    if (captureSession.canAddOutput(metadataOutput)) {
+      captureSession.addOutput(metadataOutput)
+      
+      metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+      metadataOutput.metadataObjectTypes = [.qr]
+    } else {
+      failed()
+      return
+    }
+    
+    previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+    previewLayer.frame = view.layer.bounds
+    previewLayer.videoGravity = .resizeAspectFill
+    view.layer.addSublayer(previewLayer)
+    
+    captureSession.startRunning()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    if (captureSession?.isRunning == false) {
+      captureSession.startRunning()
+    }
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    if (captureSession?.isRunning == true) {
+      captureSession.stopRunning()
+    }
+  }
+  
+  private func failed() {
+    let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
+    captureSession = nil
+  }
+  
+  private func found(code: String) {
+    print(code)
+    delegate?.qrCodeScannerViewController(self, didFoundQRCode: code)
+  }
+}
+
+
+extension QRCodeScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+  func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    captureSession.stopRunning()
+    
+    if let metadataObject = metadataObjects.first {
+      guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+      guard let stringValue = readableObject.stringValue else { return }
+      AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+      found(code: stringValue)
+    }
+    
+    dismiss(animated: true)
+  }
+}
