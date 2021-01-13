40560cbf00eed9664fb93ece1b767dff472aac79
Paolo Rocca <hispanico94@gmail.com>
2021-01-13 08:36:26 +0100

    Added Dynamic Type support for gauge (it scales with the dynamic type set)

--- SimplyAuth/SimplyAuth/Widgets/Gauge.swift
+++ SimplyAuth/SimplyAuth/Widgets/Gauge.swift
@@ -11,29 +11,28 @@ struct Gauge: View {
   var percent: CGFloat = 88
   var text: String? = nil
   var color: Color = .red
-  var width: CGFloat = 44
-  var height: CGFloat = 44
+  @ScaledMetric var gaugeSize: CGFloat = 44
   
   private var progress: CGFloat {
     (100 - percent) / 100
   }
   
   private var multiplier: CGFloat {
-    width / 44
+    gaugeSize / 44
   }
   
   var body: some View {
     ZStack {
       Circle()
         .stroke(Color(white: 0.9), style: StrokeStyle(lineWidth: 8 * multiplier))
-        .frame(width: width, height: height)
+        .frame(width: gaugeSize, height: gaugeSize)
       
       Circle()
         .trim(from: progress, to: 1)
         .stroke(color, style: StrokeStyle(lineWidth: 8 * multiplier, lineCap: .butt))
         .rotationEffect(.degrees(90))
         .rotation3DEffect(.degrees(180), axis: (x: 1.0, y: 0.0, z: 0.0))
-        .frame(width: width, height: height)
+        .frame(width: gaugeSize, height: gaugeSize)
         .animation(progress == 0 ? .none : .linear(duration: 1), value: progress)
       
       Text(text ?? "\(value: percent, using: .noDecimalsFormatter)%")
